d1fdeb6e63aebc5cce8aaa46b1c82355c522fc42
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-07-24 13:12:31 +0200

    [TASK]: Further refactoring

--- src/getHandler.nim
+++ src/getHandler.nim
@@ -11,6 +11,55 @@ from helpers import
 import types
 import constants
 
+
+###########
+# Helpers #
+###########
+
+proc showHelpAndQuit() =
+  echo """
+omt get [flags]
+
+  Gets a random value from a 'thingList' either by using a local configuraition or a provided file/string.
+
+  -d, --dry
+    Runs the command as dryrun not writing an output save file
+
+  -s=<yamlFormedListString>, --string=<yamlFormedListString>
+    Uses the provided list string instead of a config file.
+    Careful: This might still overwrite an existing omt_save.yaml if you
+    do not run the command as dryrun (see [-d])!
+
+    Example:
+      omt get -s="['a', 'b', 'c']"
+
+  -o=<nameOfOutputFile>, --output=<nameOfOutputFile>
+    Saves the configuration to the specified outputfile instead of omt_save.yaml
+  """
+  quit()
+
+proc getConfigFromFile(path: string): ThingList =
+  try:
+    let things = retrieveOMTConfigFromFile(path).thingList
+    if len(things) == 0:
+      raise newException(IOError, "The specified file does not contain a <thingList> or it is empty!")
+    else:
+      return things
+  except:
+    echo "File is no valid omt configuration!"
+    echo getCurrentException().name
+    echo getCurrentExceptionMsg()
+    quit()
+
+proc getListFromString(yamlString: string): ThingList =
+  var things: ThingList
+  load(yamlString, things)
+  return things
+
+###############
+# Main export #
+###############
+
 proc handleGet*(optParser: var OptParser): void =
   # Call randomize() once to initialize the default random number generator
   # If this is not called, the same results will occur every time these
@@ -29,77 +78,46 @@ proc handleGet*(optParser: var OptParser): void =
     of cmdShortOption, cmdLongOption:
       case optParser.key
       of "h", "help":
-        echo """
-omt get [flags]
-
-  Gets a random value from a 'thingList' either by using a local configuraition or a provided file/string.
-
-  -d, --dry
-    Runs the command as dryrun not writing an output save file
-
-  -s=<yamlFormedListString>, --string=<yamlFormedListString>
-    Uses the provided list string instead of a config file.
-    Careful: This might still overwrite an existing omt_save.yaml if you
-    do not run the command as dryrun (see [-d])!
-
-    Example:
-      omt get -s="['a', 'b', 'c']"
-
-  -o=<nameOfOutputFile>, --output=<nameOfOutputFile>
-    Saves the configuration to the specified outputfile instead of omt_save.yaml
-        """
-        quit()
+        showHelpAndQuit()
       of "d", "dry":
         dryrun = true
       of "s", "string":
-        load(optParser.val, things)
+        things = getListFromString(optParser.val)
       of "f", "file":
-        try:
-          things = retrieveOMTConfigFromFile(optParser.val).thingList
-          if len(things) == 0:
-            raise newException(IOError, "The specified file does not contain a <thingList> or it is empty!")
-        except:
-          echo "File is no valid omt configuration!"
-          echo getCurrentException().name
-          echo getCurrentExceptionMsg()
-          quit()
+        things = getConfigFromFile(optParser.val)
       of "o", "output":
-        try:
-          outputPath = optParser.val
-        except:
-          echo "Output file is invalid!"
-          echo getCurrentException().name
-          echo getCurrentExceptionMsg()
-          quit()
+        outputPath = optParser.val
       else:
         # just ignore unwanted flags
         discard
     of cmdArgument:
       raise newException(IOError, "Only a single argument is allowed!")
     of cmdEnd:
-      if len(things) == 0:
-        try:
-          # Load Config #
-          configRoot = retrieveOMTConfig()
-          things = getSavedConfigOrDefault(configRoot).thingList
-        except:
-          echo "Could not find valid configuration!\n" &
-            "Either use <" & OMT_CONFIG & ">, <" & SAVE_FILE & "> or provide string via '-s=<string>'!\n"
-          echo getCurrentException().name
-          echo getCurrentExceptionMsg()
-          quit()
       break
 
+  # Try to get valid config from files
+  if len(things) == 0:
+    try:
+      # Load Config #
+      configRoot = retrieveOMTConfig()
+      things = getSavedConfigOrDefault(configRoot).thingList
+    except:
+      echo "Could not find valid configuration!\n" &
+        "Either use <" & OMT_CONFIG & ">, <" & SAVE_FILE & "> or provide string via '-s=<string>'!\n"
+      echo getCurrentException().name
+      echo getCurrentExceptionMsg()
+      quit()
 
+  # Exit because no valid config could be found
   if things.len == 0:
     echo "List is empty!"
     quit()
+  else:
+    let sampleResult = getThingAndRest(things)
 
-  let sampleResult = getThingAndRest(things)
+    if not dryrun:
+      upateSaveFile(sampleResult.rest, configRoot, sampleResult.thing, outputPath)
 
-  if not dryrun:
-    upateSaveFile(sampleResult.rest, configRoot, sampleResult.thing, outputPath)
+    echo "Here's your thing: " & sampleResult.thing
 
-  echo "Here's your thing: " & sampleResult.thing
-
-  quit()
+    quit()
